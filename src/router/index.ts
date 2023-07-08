0beb6bd313fb3424bdad300a63a8b1f261767a96
hhmehsan <hhmehsan80@gmail.com>
2023-07-08 15:24:46 +0330

    define routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Sidebar from '../views/Sidebar.vue'
+import Dashbord from '../components/Dashbord.vue'
+import Todos from '../components/Todos.vue'
+import Profile from '../components/Profile.vue'
+import Weather from '../components/Weather.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'sidebar-parent',
+      component: Sidebar,
+      children: [
+      { path: '/dashbord', name: 'dashbord', component: Dashbord },
+      { path: '/todos', name: 'todos', component: Todos },
+      { path: '/weather', name: 'weather', component: Weather },
+      { path: '/profile', name: 'profile', component: Profile }
+    ],
     },
+ 
   ]
 })
 
