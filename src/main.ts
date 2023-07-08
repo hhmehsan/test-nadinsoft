77a38f5ab662ef37fdc24f3b349514bb9e47f5c1
hhmehsan <hhmehsan80@gmail.com>
2023-07-08 15:22:09 +0330

    import ant design and i18n in main file

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import i18n from '../src/includes/i18n'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 
 const app = createApp(App)
-app.use(Antd).mount("#app");
 app.use(router)
-
+app.use(Antd)
+app.use(i18n)
 app.mount('#app')
